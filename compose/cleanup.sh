93a58e9822753fbee1658a21d5bd38f8c3bb2d85
hanszimmer <michael.weitnau@live.de>
2020-04-01 09:31:23 +0200

    clean up

--- compose/cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker rm -f $(docker ps -aq)
-docker system prune -f --volumes
-rm -r *data || true
--- swarmWithKeepalived/setup.sh
+++ swarmWithKeepalived/setup.sh
@@ -130,12 +130,12 @@ set_scripts(){
 }
 
 set_configs(){
-    reset_config "tables" "../table_setup.sql" "/etc/table_setup.sql"
+    reset_config "tables" "./postgres/table_setup.sql" "/etc/table_setup.sql"
     
-    reset_config "prov_config" "../prov_postgresql.conf" "/etc/prov_postgresql.conf"
+    reset_config "prov_config" "./postgres/prov_postgresql.conf" "/etc/prov_postgresql.conf"
     reset_config "prov_setup" "./postgres/prov_setup.sh" "/etc/prov_setup.sh"
 
-    reset_config "sub_config" "../sub_postgresql.conf" "/etc/sub_postgresql.conf"
+    reset_config "sub_config" "./postgres/sub_postgresql.conf" "/etc/sub_postgresql.conf"
     reset_config "sub_setup" "./postgres/sub_setup.sh" "/etc/sub_setup.sh"
 
     reset_config "reconnect" "./postgres/reconnect.sh" "/etc/reconnect.sh"
