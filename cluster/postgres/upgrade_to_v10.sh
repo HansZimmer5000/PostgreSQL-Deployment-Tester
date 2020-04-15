2aba257f6cb92f71eed28adcc5d2df19577c28aa
hanszimmer <michael.weitnau@live.de>
2020-04-15 18:50:19 +0200

    edit upgrade

--- cluster/postgres/upgrade_to_v10.sh
+++ cluster/postgres/upgrade_to_v10.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# Install new Major Version
+echo "-- Install new Major Version"
 echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main 10" > /etc/apt/sources.list.d/pgdg.list
-apt-get update
-apt-get install -y --no-install-recommends --no-install-suggests postgresql-10
-#rm -rf /var/lib/apt/lists/* #There was nothing there. Why is this line here then?
+apt-get update 1>/dev/null
+apt-get install -y --no-install-recommends --no-install-suggests postgresql-10 1>/dev/null
 
-# Upgrade 
+echo "-- Upgrade"
 su - postgres -c "
 export PGBINOLD=/usr/lib/postgresql/9.5/bin
 export PGBINNEW=/usr/lib/postgresql/10/bin
@@ -14,15 +13,16 @@ export PGBINNEW=/usr/lib/postgresql/10/bin
 export PGDATAOLD=/var/lib/postgresql/9.5/data
 export PGDATANEW=/var/lib/postgresql/10/data
 
-mkdir -p "$PGDATAOLD" "$PGDATANEW"
+mkdir -p /var/lib/postgresql/9.5/data /var/lib/postgresql/10/data
 chown -R postgres:postgres /var/lib/postgresql
 
 PGDATA=/var/lib/postgresql/9.5/data /usr/lib/postgresql/9.5/bin/pg_ctl stop
 PGDATA=/var/lib/postgresql/10/data /usr/lib/postgresql/10/bin/pg_ctl init
+
 /usr/lib/postgresql/10/bin/pg_upgrade
 "
 
-# Start new Postgres
+echo "-- Start new Postgres"
 su - postgres -c "
 PGDATA=/var/lib/postgresql/10/data /usr/lib/postgresql/10/bin/pg_ctl start
 "
