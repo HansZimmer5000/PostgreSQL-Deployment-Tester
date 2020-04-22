c4970f3ab17b14a9814015949af7c99a01b639a0
hanszimmer <michael.weitnau@live.de>
2020-04-22 14:35:51 +0200

    set new ips and remove -e

--- cluster/postgres/sub_setup.sh
+++ cluster/postgres/sub_setup.sh
@@ -43,8 +43,6 @@ init_this_subscriber() {
 
     echo "Using IP ($SUBSCRIBER_IP) and ID ($SUBSCRIPTION_ID)"
 
-    set -e
-
     echo "host  replication  all  0.0.0.0/0  md5" >> /var/lib/postgresql/data/pg_hba.conf
 
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "
