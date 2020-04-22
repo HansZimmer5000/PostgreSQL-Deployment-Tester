942928a277e529593d93bb61f96ac0130a34174f
hanszimmer <michael.weitnau@live.de>
2020-04-22 17:34:23 +0200

    edit upgrade

--- cluster/postgres/upgrade_to_v10.sh
+++ cluster/postgres/upgrade_to_v10.sh
@@ -6,14 +6,16 @@ apt-get update 1>/dev/null
 apt-get install -y --no-install-recommends --no-install-suggests postgresql-10 1>/dev/null
 
 echo "-- Upgrade"
+mkdir -p /var/lib/postgresql/9.5/data /var/lib/postgresql/10/data
+chown -R postgres:postgres /var/lib/postgresql
+
 su - postgres -c "
 export PGBINOLD=/usr/lib/postgresql/9.5/bin
 export PGBINNEW=/usr/lib/postgresql/10/bin
 export PGDATAOLD=/var/lib/postgresql/9.5/data
 export PGDATANEW=/var/lib/postgresql/10/data
-mkdir -p /var/lib/postgresql/9.5/data /var/lib/postgresql/10/data
-chown -R postgres:postgres /var/lib/postgresql
-PGDATA=/var/lib/postgresql/9.5/data /usr/lib/postgresql/9.5/bin/pg_ctl stop
+
+PGDATA=/var/lib/postgresql/9.5/data /usr/lib/postgresql/9.5/bin/pg_ctl stop # TODO this will kill container PID 1 == Container stops, how to work around?
 PGDATA=/var/lib/postgresql/10/data /usr/lib/postgresql/10/bin/pg_ctl init
 /usr/lib/postgresql/10/bin/pg_upgrade
 "
--- cluster/test_scripts/test_scenarios.sh
+++ cluster/test_scripts/test_scenarios.sh
@@ -237,7 +237,6 @@ test_4(){
 
 # $1 = Node, $2 = Container ID
 upgrade(){
-    # TODO: Breaks! Files needs to be in Container and Executable!
     $SSH_CMD root@$1 "docker exec -t -u root $2 /etc/upgrade_to_v10.sh" 
 }
 
