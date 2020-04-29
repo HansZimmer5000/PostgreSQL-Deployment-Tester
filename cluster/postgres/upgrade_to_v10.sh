9376a7c7f0746b30e7503e7ee52af1801e87d2ee
hanszimmer <michael.weitnau@live.de>
2020-04-29 18:03:29 +0200

    add reconnect instead of new init

--- cluster/postgres/upgrade_to_v10.sh
+++ cluster/postgres/upgrade_to_v10.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "-- Install new Major Version"
 # TODO Some of the installed packages needs READLINE, I guess to select wheter version to install.
@@ -23,13 +23,42 @@ PGDATA=/var/lib/postgresql/10/data /usr/lib/postgresql/10/bin/initdb -E 'UTF-8' 
 
 # Reuse old configuratoin file
 cp /var/lib/postgresql/9.5/data/postgresql.conf /var/lib/postgresql/10/data/postgresql.conf
+cp /var/lib/postgresql/9.5/data/pg_hba.conf /var/lib/postgresql/10/data/pg_hba.conf
 
 /usr/lib/postgresql/10/bin/pg_upgrade
 "
 
+echo "-- Setup Postgres (PGLogical)"
+
+get_ip() {
+    #IPs=($(hostname -I)) # Konvertiert Leerzeichen getrennten Text direkt zu Array, TODO Überall einführen wo sinnvoll & leerzeichen Listen genutzt!
+    IPs=($(ifconfig eth1 | grep "inet"))
+    echo ${IPs[1]}
+}
+
+reconnect() {
+    echo "Next line may fail if database was not able to start right in the first place"
+
+    psql -e -v ON_ERROR_STOP=1 --username postgres --dbname testdb -c "SELECT pglogical.drop_subscription('$1');"
+
+    psql -e -v ON_ERROR_STOP=1 --username postgres --dbname testdb -c "SELECT pglogical.create_subscription(subscription_name := '$1',provider_dsn := 'host=192.168.1.149 port=5433 dbname=testdb password=pass user=postgres');"
+
+    psql -e -v ON_ERROR_STOP=1 --username postgres --dbname testdb -c "SELECT pglogical.wait_for_subscription_sync_complete('$1');"
+
+    psql -e -v ON_ERROR_STOP=1 --username postgres --dbname testdb -c "SELECT pglogical.show_subscription_status('$1');"
+
+}
+
+PGDATA=/var/lib/postgresql/10/data
+echo "Current PGDATA: $PGDATA"
+echo "host  replication  all  0.0.0.0/0  md5" >> $PGDATA/pg_hba.conf
+
 echo "-- Start new Postgres"
 su - postgres -c "
 PGDATA=/var/lib/postgresql/10/data /usr/lib/postgresql/10/bin/pg_ctl start
-" 
+"
 
-#TODO setup pglogical again as before (via sub_setup. Does this need updating to fit to v10?)
+#init_this_subscriber
+SUBSCRIBER_IP=$(get_ip)
+SUBSCRIPTION_ID="${SUBSCRIBER_IP//./}"
+reconnect "subscription$SUBSCRIPTION_ID"
