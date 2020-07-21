0e3871b929a12edb6e59ab64aaa55bbc523bbddf
HansZimmer <michael.weitnau@live.de>
2020-07-21 16:18:37 +0200

    update print

--- cluster/setup_scripts/vm.sh
+++ cluster/setup_scripts/vm.sh
@@ -6,9 +6,9 @@
 # - ssh_scp.sh
 
 get_current_node_ips() {
-    if ! [ -z "$dsn1_node" ]; then echo dsn1_node: $($SSH_CMD root@$dsn1_node hostname -I); fi
-    if ! [ -z "$dsn2_node" ]; then echo dsn2_node: $($SSH_CMD root@$dsn2_node hostname -I); fi
-    if ! [ -z "$dsn3_node" ]; then echo dsn3_node: $($SSH_CMD root@$dsn3_node hostname -I); fi
+    if ! [ -z "$dsn1_node" ]; then echo "dsn1 (label=$(get_label_version 1))": $($SSH_CMD root@$dsn1_node hostname -I); fi
+    if ! [ -z "$dsn2_node" ]; then echo "dsn2 (label=$(get_label_version 2))": $($SSH_CMD root@$dsn2_node hostname -I); fi
+    if ! [ -z "$dsn3_node" ]; then echo "dsn3 (label=$(get_label_version 3))": $($SSH_CMD root@$dsn3_node hostname -I); fi
 }
 
 wait_for_vm() {
