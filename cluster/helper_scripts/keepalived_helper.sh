4a85a88351871413d7b469078fb9d86e7ddf9e49
HansZimmer <michael.weitnau@live.de>
2020-08-20 14:39:08 +0200

    add doc and clean up keepalived helper

--- cluster/helper_scripts/keepalived_helper.sh
+++ cluster/helper_scripts/keepalived_helper.sh
@@ -3,17 +3,23 @@
 # Depends on (will be sourced by using script):
 # - ssh_scp.sh
 
+# prepare_keepalived will prepare Keepalived for the Postgres Cluster
+# Context: SETUP
 prepare_keepalived(){
     set_keepalived_files
     allow_keepalived_selinux
 }
 
+# allow_keepalived_selinux will allow Keepalived execution in selinux
+# Context: SETUP
 allow_keepalived_selinux() {
     # Additionally in current configuration:
     # in "/etc/sysconfig/selinux" is: SELINUX=disabled (needed restart)
     ssh_cmd_for_each_node "setenforce 0"
 }
 
+# set_keepalived_files will copy neccessary files to the Docker Swarm nodes.
+# Context: SETUP
 set_keepalived_files() {
     scp_cmd_for_each_node ./keepalived/check.sh /etc/keepalived/
     scp_cmd_for_each_node ./keepalived/promote.sh /etc/keepalived/
@@ -29,6 +35,8 @@ set_keepalived_files() {
     ssh_cmd_for_each_node "systemctl restart keepalived"
 }
 
+# start_keepalived will start Keepalived on all Docker Swarm nodes.
+# Context: SETUP
 start_keepalived() {
     ssh_cmd_for_each_node "systemctl start keepalived"
 }
