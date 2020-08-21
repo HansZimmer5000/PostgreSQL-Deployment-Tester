4b2fef8e82a794070323c37b9652af7e4a0b200a
HansZimmer <michael.weitnau@live.de>
2020-08-21 13:36:22 +0200

    add push

--- merge.sh
+++ merge.sh
@@ -6,6 +6,7 @@ merge_branch_targets="master feature-compose"
 for branch in $merge_branch_targets; do
     git checkout $branch
     git merge $merge_branch_source
+    git push
 done
 
 git checkout $merge_branch_source
