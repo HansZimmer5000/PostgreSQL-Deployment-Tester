1e6815fbecb084b35ea0f464173db9de82fafbff
HansZimmer <michael.weitnau@live.de>
2020-08-25 10:36:27 +0200

    add shutdown

--- compose_cluster/setup.sh
+++ compose_cluster/setup.sh
@@ -14,17 +14,20 @@ This script sets up the environment (docker compose) to start a PostgreSQL Clust
 --------------------
 Flags:
 -p  will start the postgres cluster at last.
+-s  will shutdown the postgres cluster
 -h  will print this help
 "
 }
 
 postgres_is_not_running=false
+shutdown=true
 
 while getopts 'hmsp' opts; do
     case ${opts} in 
         h)  print_setup_help 
             exit 0 ;;
         p)  postgres_is_not_running=true  ;;
+        s)  shutdown=true ;;
     esac
 done
 
@@ -33,6 +36,7 @@ echo "
 ----------------------------------
 with values:
 postgres_is_not_running=$postgres_is_not_running
+shutdown=$shutdown
 "
 
 if $postgres_is_not_running; then
@@ -48,6 +52,10 @@ if $postgres_is_not_running; then
     echo "-- Bringing Compose files up"
     docker-compose -f stacks/stack95_compose.yml up --scale db95=1 -d 1> /dev/null 
     docker-compose -f stacks/stack10_compose.yml up --scale db10=0 -d 1> /dev/null
+elif $shutdown; then    
+    echo "-- Shutting down"
+    docker-compose -f stacks/stack95_compose.yml rm -fs 1> /dev/null
+    docker-compose -f stacks/stack10_compose.yml rm -fs 1> /dev/null
 fi
 
 echo "
