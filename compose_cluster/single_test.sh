c9ee3456a326053ca289a4560f2bd63df2a82470
HansZimmer <michael.weitnau@live.de>
2020-08-26 15:29:53 +0200

    working  proof of concept and notes

--- compose_cluster/single_test.sh
+++ compose_cluster/single_test.sh
@@ -21,31 +21,31 @@ echo "-- Fill Old Postgres"
 docker exec -it \
 	-u postgres \
 	stacks_db95_1 \
-	pgbench -i -s 10 
+	pgbench -i -s 2 
 
 echo "-- Crash Old Postgres"
-remove_old_provider stacks_db95_1
+#remove_old_provider stacks_db95_1
 kill_pg_by_name stacks_db95_1 smart
 update_id_ip_nodes
 print_id_ip_nodes
 
-echo "-- Updating Volume"
-docker run --rm \
-    -v stacks_pgdata95:/var/lib/postgresql/9.5/data \
-    -v stacks_pgdata10:/var/lib/postgresql/10/data \
-	"tianon/postgres-upgrade:9.5-to-10" \
-	--link
+#echo "-- Updating Volume"
+#docker run -it --rm \
+#    -v stacks_pgdata95:/var/lib/postgresql/9.5/data \
+#    -v stacks_pgdata10:/var/lib/postgresql/10/data \
+#	-v $(pwd)/postgres/sub_postgresql.conf:/usr/share/postgresql/postgresql.conf.sample \
+#	-v $(pwd)/postgres/sub_postgresql.conf:/usr/share/postgresql/10/postgresql.conf.sample \
+#	"hanszimmer5000/postgres-upgrade:9.5-to-10" bash
 
-echo "-- Start New Postgres"
-docker run -dit \
-	--name postgres-upgrade-testing \
-	-v stacks_pgdata10:/var/lib/postgresql/data \
-	"postgres:10" 
-
-docker exec postgres-upgrade-testing ls /var/lib/postgresql/data
-docker exec postgres-upgrade-testing cat /var/lib/postgresql/data/PG_VERSION
-
-exit 0
+# Notes for Update Success
+# - Needs right postgresql.config (shared librarier)
+# - Need right volume mount points
+# - maybe execute pg_upgrade via root
+#	- but then check locale
+#	- but then check encoding (maybe already done with locale)
+# - set all variables and PGDATA correct
+# - new data must be initdb before
+# - install needed librariers via apt-get (pglogical 9.5 / postgres 9.5)
 
 echo "-- Start New Postgres"
 start_new_subscriber 10
