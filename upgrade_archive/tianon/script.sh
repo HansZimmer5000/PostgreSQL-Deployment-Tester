bf38ecf4073c6a5fdae6431323d09c9d5dd97158
HansZimmer <michael.weitnau@live.de>
2020-08-26 14:39:53 +0200

    fix simple test

--- upgrade_archive/tianon/script.sh
+++ upgrade_archive/tianon/script.sh
@@ -10,8 +10,6 @@ sleep 1s
 OLD='9.5'
 NEW='10'
 sleep_time=10
-old_image="postgres:$OLD"
-new_image="postgres:$NEW"
 
 echo "-- Building Upgrade Image"
 docker build image -f image/Dockerfile -t hanszimmer5000/postgres-upgrade:$OLD-to-$NEW 1> /dev/null
@@ -24,10 +22,10 @@ echo "-- Starting Old Image"
     #-v ../../compose_cluster/postgres/sub_postgresql.conf:/usr/share/postgresql/postgresql.conf. sample \
 	#-e PGDATA=/var/lib/postgresql/9.5/data\
 docker run -dit \
-    -e POSTGRES_PASSWORD=pass \
 	--name postgres-upgrade-testing \
+    -e POSTGRES_PASSWORD=pass \
 	-v "$PWD/$OLD/data":/var/lib/postgresql/data \
-	"$old_image" 1> /dev/null
+	"postgres:$OLD" 1> /dev/null
 
 sleep $sleep_time
 docker logs --tail 100 postgres-upgrade-testing
@@ -39,21 +37,24 @@ docker exec -it \
 	postgres-upgrade-testing \
 	pgbench -i -s 10 1> /dev/null
 
-#docker stop postgres-upgrade-testing 1> /dev/null
+#WORKS (clean shutdown case): docker stop postgres-upgrade-testing 1> /dev/null
+#WORKS (clean shutdown case): #docker exec -u postgres postgres-upgrade-testing pg_ctl stop -m smart
 docker rm -f postgres-upgrade-testing 1> /dev/null
 
 echo "-- Starting Upgrade"
 set -e
-docker run -it --rm \
+docker run --rm \
 	-v "$PWD/$OLD/data":/var/lib/postgresql/9.5/data \
 	-v "$PWD/$NEW/data":/var/lib/postgresql/10/data \
-	"hanszimmer5000/postgres-upgrade:$OLD-to-$NEW" bash
+	"hanszimmer5000/postgres-upgrade:$OLD-to-$NEW" #bash
 
 echo "-- Starting New Image"
 docker run -dit \
-	-e POSTGRES_PASSWORD=password \
+	-e POSTGRES_PASSWORD=pass \
 	--name postgres-upgrade-testing \
 	-v "$PWD/$NEW/data":/var/lib/postgresql/data \
-	"$new_image" 1> /dev/null
+	"postgres:$NEW" #bash
 sleep $sleep_time
 docker logs --tail 10 postgres-upgrade-testing
+
+#sudo chown -R $(whoami) .
